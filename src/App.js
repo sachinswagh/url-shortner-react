f9c8c2cdb4c7a50bef754b2b79fccd338af4be08
Sachin S Wagh <sachin.wagh@opcito.com>
2019-03-14 23:04:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import FacebookLogin from 'react-facebook-login';
+
+import GoogleLogin from 'react-google-login';
+
 class App extends Component {
+
   render() {
+
+    // componentDidMount
+
+    const responseFacebook = (response) => {
+      console.log(response);
+    }
+
+    const responseGoogle = (response) => {
+      console.log(response);
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>LOGIN WITH FACEBOOK AND GOOGLE</h1>
+
+      <FacebookLogin
+        appId="2364857773737783" //APP ID NOT CREATED YET
+        fields="name,email,picture"
+        callback={responseFacebook}
+      />
+      <br />
+      <br />
+
+
+      <GoogleLogin
+        clientId="654527843128-ck8kbtd2idmpu679ajakt9mbs71r0r1f.apps.googleusercontent.com" //CLIENTID NOT CREATED YET
+        buttonText="LOGIN WITH GOOGLE"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+      />
+
       </div>
     );
   }
 }
 
 export default App;
+
+
+//// fb: appSecret = 4555762ff77d8180293432d7039a90d0
+//// ggl: clientId:
+//// ggl: clientSecret: BAv6NvgHx8RT1tLtGK9LyRtK
